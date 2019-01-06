a25acad314d273809bed305eb3ae455ba8f77de8
Sugarbabyzz <406857586@qq.com>
2019-01-06 10:59:26 +0800

    Update chapter4_analyse_library_XPath.py

--- Spider/chapter4_analyse_library_XPath.py
+++ Spider/chapter4_analyse_library_XPath.py
@@ -2,6 +2,7 @@
 
 #****** 4.1 使用XPath ******
 # XML Path Language， 即XML路径语言，它是一门在XML文档中查找信息的语言，用来搜寻XML文档的，也适用于HTML文档的搜索
+# XPath中提供100多个函数，具体作用参考：http://www.w3school.com/cn/xpath/xpath_functions.asp
 
 # 1、XPath概述
 
@@ -109,4 +110,35 @@ text = '''
 '''
 html = etree.HTML(text)
 result = html.xpath('//li[contains(@class, "li") and @name="item"]/a/text()')
+print(result)
+
+# 13、按序选择
+#   同时匹配多个节点，而只要其中某个节点
+#   可以利用中括号中传入索引的方法获取特定次序的节点
+#   下标是从1开始的
+
+text = '''
+<div>
+<ul>
+<li class="item-0"><a href="link1.html">first item</a></li>
+<li class="item-1"><a href="link2.html">second item</a></li>
+<li class="item-inactive"><a href="link3.html">third item</a></li>
+<li class="item-1"><a href="link4.html">fourth item</a></li>
+<li class="item-0"><a href="link5.html">fifth item</a>
+</ul>
+</div>
+'''
+
+html = etree.HTML(text)
+#   选取第一个li节点
+result = html.xpath('//li[1]/a/text()')
+print(result)
+#   选取最后一个li节点
+result = html.xpath('//li[last()]/a/text()')
+print(result)
+#   选取位置小于3的li节点
+result = html.xpath('//li[position()<3]/a/text()')
+print(result)
+#   选取倒数第三个li节点
+result = html.xpath('//li[last()-2]/a/text()')
 print(result)
