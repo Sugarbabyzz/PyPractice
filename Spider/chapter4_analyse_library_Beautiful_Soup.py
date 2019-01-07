6d9253c73a2c12e13549d811db95d9b2ab6f4e28
Sugarbabyzz <406857586@qq.com>
2019-01-07 11:35:03 +0800

    Update chapter4_analyse_library_Beautiful_Soup.py

--- Spider/chapter4_analyse_library_Beautiful_Soup.py
+++ Spider/chapter4_analyse_library_Beautiful_Soup.py
@@ -240,3 +240,44 @@ print(soup.find_all(text=re.compile('link')))
 #   2）find()
 #   只返回第一个匹配的元素，类型是Tag
 #   方法见书P181
+
+# 7、CSS选择器
+#   调用select()方法，传入相应的CSS选择器即可
+html = '''
+<div class="panel">
+<div class="panel-heading">
+<h4>Hello</h4>
+</div>
+<div class="panel-body">
+<ul class="list" id="list-1" name="elements">
+<li class="element">Foo</li>
+<li class="element">Bar</li>
+<li class="element">Jay</li>
+</ul>
+<ul class="list list-small" id="list-2">
+<li class="element">Foo</li>
+<li class="element">Bar</li>
+</ul>
+</div>
+</div>
+'''
+soup = BeautifulSoup(html, 'lxml')
+print(soup.select('.panel .panel-heading'))
+print(soup.select('ul li'))
+print(soup.select('#list-2 .element'))
+print(type(soup.select('ul')[0]))
+
+# *嵌套选择
+for ul in soup.select('ul'):
+    print(ul.select('li'))
+
+# *获取属性
+for ul in soup.select('ul'):
+    print(ul['id'])
+    print(ul.attrs['id'])
+
+# *获取文本
+#   除了.string，还可以get_text()
+for li in soup.select('li'):
+    print('Get Text:', li.get_text())
+    print('String:', li.string)
