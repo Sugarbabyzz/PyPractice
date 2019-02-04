330372a17028017c3663c7944d3d87994b5390cd
Sugarbabyzz <406857586@qq.com>
2019-02-04 16:46:17 +0800

    Update chapter5_data_store_csv.py

--- Spider/chapter5_data_store_csv.py
+++ Spider/chapter5_data_store_csv.py
@@ -16,10 +16,26 @@ with open('data.csv', 'w') as csvfile:
     writer.writerow(['10002', 'Bob', '22'])
     writer.writerow(['10003', 'Jordan', '21'])
 
-#   以空格分隔
+#   delimiter参数，以空格分隔
 with open('data.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=' ')
     writer.writerow(['id', 'name', 'age'])
     writer.writerow(['10001', 'Mike', '20'])
     writer.writerow(['10002', 'Bob', '22'])
     writer.writerow(['10003', 'Jordan', '21'])
+
+#   调用writerows写入多行
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ')
+    writer.writerow(['id', 'name', 'age'])
+    writer.writerows([['10001', 'Mike', '20'], ['10002', 'Bob', '22'], ['10003', 'Jordan', '21']])
+
+#   字典方式写入
+with open('data.csv', 'w') as csvfile:
+    fieldnames = ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow(['10001', 'Mike', '20'])
+    writer.writerow(['10002', 'Bob', '22'])
+    writer.writerow(['10003', 'Jordan', '21'])
+
