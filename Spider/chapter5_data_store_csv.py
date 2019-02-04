5f8f925a53e650744af83f0e8a287ec393f4d1c9
Sugarbabyzz <406857586@qq.com>
2019-02-04 17:00:48 +0800

    Update chapter5

--- Spider/chapter5_data_store_csv.py
+++ Spider/chapter5_data_store_csv.py
@@ -30,12 +30,39 @@ with open('data.csv', 'w') as csvfile:
     writer.writerow(['id', 'name', 'age'])
     writer.writerows([['10001', 'Mike', '20'], ['10002', 'Bob', '22'], ['10003', 'Jordan', '21']])
 
-#   字典方式写入
+#   字典方式写入,分隔符默认逗号
 with open('data.csv', 'w') as csvfile:
     fieldnames = ['id', 'name', 'age']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerow(['10001', 'Mike', '20'])
-    writer.writerow(['10002', 'Bob', '22'])
-    writer.writerow(['10003', 'Jordan', '21'])
+    writer.writerow({'id': '10001', 'name': 'Mike', 'age': 20})
+    writer.writerow({'id': '10002', 'name': 'Bob', 'age': 22})
+    writer.writerow({'id': '10003', 'name': 'Jordan', 'age': 21})
+
+#   追加内容
+with open('data.csv', 'a') as csvfile:
+    fieldnames = ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({'id': '10004', 'name': 'Durant', 'age': 22})
+
+#   中文编码问题
+with open('data.csv', 'a', encoding='utf-8') as csvfile:
+    fieldnames = ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({'id': '10004', 'name': '王伟', 'age': 22})
+
+
+# 2、读取
+with open('data.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+
+#   read_csv()方法
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+print(df)
+
 
