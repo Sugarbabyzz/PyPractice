7969ff3c749d8b46403b88dee35a75b1964efccc
Sugarbabyzz <406857586@qq.com>
2019-02-07 11:41:24 +0800

    Update chapter5_data_store_redis.py

--- Spider/chapter5_data_store_redis.py
+++ Spider/chapter5_data_store_redis.py
@@ -5,3 +5,21 @@
 
 # 1、安装Redis及redis-py库，要做数据导入/导出操作，还需要RedisDump
 
+# 2、redis-py提供Redis和StrictRedis库
+#    推荐使用StrictRedis库
+
+# 3、连接Redis
+#   1)使用StrictRedis连接
+from redis import StrictRedis
+
+redis = StrictRedis(host='localhost', port=6379, db=0, password='123456')
+#   调用set()方法，设置一个键值对，然后获取并打印
+redis.set('name', 'Bob')
+print(redis.get('name'))
+
+#   2）使用ConnectionPool连接
+from redis import ConnectionPool
+
+pool = ConnectionPool(host='localhost', port=6379, db=0, password='123456')
+redis = StrictRedis(connection_pool=pool)
+#   3）使用URL连接
