4cd0f25d33278542e2549a21b6045da699313ae2
Sugarbabyzz <406857586@qq.com>
2019-02-07 12:57:09 +0800

    Update chapter6_ajax.py

---
+++ Spider/chapter6_ajax.py
@@ -0,0 +1,15 @@
+
+
+#****** 6.1 Ajax ******
+
+# Asynchronous JavaScript and XML
+# requests获取的是原始HTML文档，页面可能经过JavaScript处理数据后生成，也可能通过Ajax加载
+
+# 1、原理
+#   1）发送请求
+#   2）解析内容
+#   3）渲染网页
+
+#****** 6.2 Ajax分析方法 ******
+
+
