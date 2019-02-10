2db5d98ab988b391ea6ca5feb6d79f6950069d20
Sugarbabyzz <406857586@qq.com>
2019-02-10 14:03:32 +0800

    Update chapter6_ajax.py

--- Spider/chapter6_ajax.py
+++ Spider/chapter6_ajax.py
@@ -12,4 +12,47 @@
 
 #****** 6.2 Ajax分析方法 ******
 
+# 1、查看请求
+# 2、过滤请求
+
+#****** 6.3 Ajax结果提取 ******
+
+# 1、分析请求
+#   打开Chrome的XHR过滤器，选定其中一个请求，分析参数
+# 2、分析响应
+#   只有page是可变参数
+# 3、实战演练
+#   获取前十页微博
+# https://m.weibo.cn/api/container/getIndex?type=uid&value=2830678474&containerid=1076032830678474&page=2
+
+from urllib.parse import urlencode
+import requests
+
+base_url = 'https://m.weibo.cn/api/container/getIndex?'
+
+headers = {
+    'Host': 'm.weibo.cn',
+    'Referer': 'https://m.weibo.cn/u/2830678474',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
+def get_page(page):
+    params = {
+        'type': 'uid',
+        'value': '2830678474',
+        'containerid': '1076032830678474',
+        'page': page
+    }
+    url = base_url + urlencode(params)
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+    except requests.ConnectionError as e:
+        print('Error', e.args)
+
+
+
+
 
