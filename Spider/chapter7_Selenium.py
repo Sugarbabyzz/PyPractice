95e0b1dae1b04dfa3bde769b0603ae84d37bfd30
Sugarbabyzz <406857586@qq.com>
2019-04-04 09:08:06 +0800

    Update chapter7_Selenium.py

--- Spider/chapter7_Selenium.py
+++ Spider/chapter7_Selenium.py
@@ -153,8 +153,16 @@ print(logo.get_attribute('href'))
 # * 获取文本值
 
 '''
+from selenium import webdriver
 
+browser = webdriver.Chrome()
+url = 'http://www.zhihu.com/explore'
+browser.get(url)
+input = browser.find_element_by_class_name('zu-top-add-question')
+print(input.text)
 '''
+
+# * 
 
 
 
