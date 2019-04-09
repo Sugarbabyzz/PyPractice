95e2370318438b6696eb1cdfaea5389525e5aad7
Sugarbabyzz <406857586@qq.com>
2019-04-09 10:01:20 +0800

    request test

--- Spider/images360/images360/spiders/images.py
+++ Spider/images360/images360/spiders/images.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import urlencode
+from scrapy import Spider, Request
+
 import scrapy
 
 
@@ -6,6 +9,16 @@ class ImagesSpider(scrapy.Spider):
     name = 'images'
     allowed_domains = ['images.so.com']
     start_urls = ['http://images.so.com/']
+
+    def start_requests(self):
+        data = {'ch': 'photography', 'listtype': 'new', 'temp': 1}
+        base_url = 'https://images.so.com/zj?'
+        for page in range(1, self.settings.get('MAX_PAGE') + 1):
+            data['sn'] = page * 30
+            params = urlencode(data)
+            url = base_url + params
+            yield Request(url, self.parse)
+
 
     def parse(self, response):
         pass
