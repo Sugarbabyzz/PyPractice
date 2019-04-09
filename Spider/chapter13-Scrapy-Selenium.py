cf9f41e5436d63dc126ea357d68f95a86edb0979
Sugarbabyzz <406857586@qq.com>
2019-04-09 14:26:01 +0800

    Create chapter13-Scrapy-Selenium.py

---
+++ Spider/chapter13-Scrapy-Selenium.py
@@ -0,0 +1,13 @@
+
+
+#****** 13.5 Downloader Middleware的用法 ******
+
+# 对接项目-scrapyseleniumtest
+
+#   Scrapy对接Selenium，以PhantomJS进行演示
+#   PhantomJS是一个无界面、可脚本编程的WebKit浏览器引擎。
+#   Selenium支持PhantomJS，运行时就不会弹出一个浏览器了，且运行效率较高
+
+
+
+
