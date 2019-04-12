4bc203756579c726296e43b0faa7b33b72b6a74b
Sugarbabyzz <406857586@qq.com>
2019-04-12 12:31:14 +0800

    Update db.py

--- Spider/chapter9_ProxyPool/db.py
+++ Spider/chapter9_ProxyPool/db.py
@@ -1,3 +1,8 @@
+
+
+import redis
+from random import choice
+
 MAX_SCORE = 100
 MIN_SCORE = 0
 INITIAL_SCORE = 10
@@ -6,8 +11,6 @@ REDIS_PORT = 6379
 REDIS_PASSWORD = None
 REDIS_KEY = 'proxies'
 
-import redis
-from random import choice
 
 class RedisClient(object):
     def __init__(self, host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD):
@@ -28,3 +31,65 @@ class RedisClient(object):
         """
         if not self.db.zscore(REDIS_KEY, proxy):
             return self.db.zadd(REDIS_KEY, score, proxy)
+
+    def random(self):
+        """
+        随机获取有效代理，首先尝试获取最高分数代理，如果不存在，则按照排名获取，否则异常
+        :return: 随机代理
+        """
+        result = self.db.zrangebyscore(REDIS_KEY, MAX_SCORE, MAX_SCORE)
+        if len(result):
+            return choice(result)
+        else:
+            result = self.db.zrangebyscore(REDIS_KEY, 0, 100)
+            if len(result):
+                return choice(result)
+            else:
+                raise PoolEmptyError
+
+    def decrease(self, proxy):
+        """
+        代理值减一分，分数小于最小值则删除
+        :param proxy: 代理
+        :return: 修改后的代理分数
+        """
+        score = self.db.zscore(REDIS_KEY, proxy)
+        if score and score > MIN_SCORE:
+            print('代理', proxy, '当前分数', score, '减1')
+            return self.db.zincrby(REDIS_KEY, proxy, -1)
+        else:
+            print('代理', proxy, '当前分数', score, '移除')
+            return self.db.zrem(REDIS_KEY, proxy)
+
+    def exists(self, proxy):
+        """
+        判断是否存在
+        :param proxy: 代理
+        :return: 是否存在
+        """
+        return not self.db.zscore(REDIS_KEY, proxy) == None
+
+    def max(self, proxy):
+        """
+        将代理设置为MAX_SCORE
+        :param proxy: 代理
+        :return: 设置结果
+        """
+        print('代理', proxy, '可用，设置为', MAX_SCORE)
+        return self.db.zadd(REDIS_KEY, MAX_SCORE, proxy)
+
+    def count(self):
+        """
+        获取数量
+        :return: 数量
+        """
+        return self.db.zcard(REDIS_KEY)
+
+    def all(self):
+        """
+        获取全部代理
+        :return: 全部代理列表
+        """
+        return self.db.zrangebyscore(REDIS_KEY, MIN_SCORE, MAX_SCORE)
+
+
